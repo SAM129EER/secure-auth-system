a3da997c0656f07908807b9663757ec2469c8b71
SameerJangid <jangidsameer77@gmail.com>
2026-02-07 20:58:38 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-import "./src/config/env.js";  //this should be on top
-import  "./src/DB/redis.js";
+import "./src/config/env.js"; //this should be on top
+import "./src/DB/redis.js";
 // console.log(process.env.REDIS_URL);
 
 import connectDB from "./src/DB/db.js";
@@ -7,10 +7,9 @@ import app from "./src/app.js";
 const PORT = process.env.PORT || 5000;
 
 try {
+  await connectDB();
 
-    await connectDB();
-   
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 } catch (error) {
-    console.log(error);
+  console.log(error);
 }
